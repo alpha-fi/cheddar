a1b2beadc94f2655114ee1af3736ad56343f94b1
luciotato <luciotato@gmail.com>
2021-06-18 10:42:31 -0300

    script set-start-end.sh

--- deploy-p1.sh
+++ deploy-p1.sh
@@ -22,10 +22,10 @@ FARMING_END=1623700800
 #  read input
 #  near delete $CONTRACT_ACC $MASTER_ACC
 #  near create-account $CONTRACT_ACC --masterAccount $MASTER_ACC
- near deploy $CONTRACT_ACC $WASM new "{\"owner_id\":\"$OWNER\", \"cheddar_id\":\"token.$MASTER_ACC\",\"reward_rate\":\"$YOCTO_CHEDDAR_PER_SECOND_PER_NEAR\", \"farming_start\":$FARMING_START, \"farming_end\":$FARMING_END}" --accountId $MASTER_ACC
+# near deploy $CONTRACT_ACC $WASM new "{\"owner_id\":\"$OWNER\", \"cheddar_id\":\"token.$MASTER_ACC\",\"reward_rate\":\"$YOCTO_CHEDDAR_PER_SECOND_PER_NEAR\", \"farming_start\":$FARMING_START, \"farming_end\":$FARMING_END}" --accountId $MASTER_ACC
 
 ##redeploy only
-#near deploy $CONTRACT_ACC $WASM  --accountId $MASTER_ACC
+near deploy $CONTRACT_ACC $WASM  --accountId $MASTER_ACC
 #near call $CONTRACT_ACC set_rewards_per_year {\"new_value\":12500} --accountId $MASTER_ACC
 #near call token.$MASTER_ACC mint "{\"account_id\":\"$CONTRACT_ACC\",\"amount\":\"100$E6$YOCTO\"}" --amount 0.000000000000000000000001 --accountId $MASTER_ACC
 #near call $CONTRACT_ACC open {\"value\":true} --accountId $MASTER_ACC
