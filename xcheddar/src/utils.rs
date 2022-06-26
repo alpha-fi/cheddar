428f503dc3d64c05daf2c995d5815791923a7c2c
YellingOilbird <@guacharo.w3@yahoo.com>
2022-06-26 16:43:44 +0400

    callbacks update

--- xcheddar/src/utils.rs
+++ xcheddar/src/utils.rs
@@ -1,11 +1,10 @@
 use chrono::prelude::*;
-use near_sdk::json_types::U128;
-use near_sdk::{ext_contract, Balance, Gas, Timestamp};
+use near_sdk::{Balance, Gas, Timestamp};
 
 use uint::construct_uint;
 
 pub const CHEDDAR_DECIMALS: u8 = 24;
-pub const NO_DEPOSIT: u128 = 0;
+pub const ONE_YOCTO: u128 = 1;
 pub const GAS_FOR_RESOLVE_TRANSFER: Gas = Gas(10_000_000_000_000);
 pub const GAS_FOR_FT_TRANSFER: Gas = Gas(20_000_000_000_000);
 
@@ -21,6 +20,7 @@ pub const ERR_EMPTY_TOTAL_SUPPLY: &str = "Total supply cannot be empty!";
 pub const ERR_KEEP_AT_LEAST_ONE_XCHEDDAR: &str = "At least 1 Cheddar must be on lockup contract account";
 pub const ERR_MISMATCH_TOKEN: &str = "Only Cheddar tokrn contract may calls this lockup contract";
 pub const ERR_PROMISE_RESULT: &str = "Expected 1 promise result";
+pub const ERR_WRONG_TRANSFER_MSG: &str = "Use empty msg for deposit to stake Cheddar or msg = 'reward' for add some reward to contract";
 
 construct_uint! {
     // 256-bit unsigned integer.
@@ -37,27 +37,5 @@ pub fn convert_from_yocto_cheddar(yocto_amount: Balance) -> u128 {
 pub fn convert_timestamp_to_datetime(timestamp: u32) -> DateTime<Utc> {
     let naive_datetime = NaiveDateTime::from_timestamp(timestamp.into(), 0);
     DateTime::from_utc(naive_datetime, Utc)
-} 
-
-/// U can impl this function from cheddar vesting locking to calculate minted and unlocked in xcheddar.rs
-/// This contract of xCheddar using same logic to count amounts of locked tokens
-/* 
-///returns amount * numerator/denominator
-pub fn fraction_of(amount: u128, numerator: u128, denominator: u128) -> u128 {
-    return (U256::from(amount) * U256::from(numerator) / U256::from(denominator)).as_u128();
 }
- */
-
-
-//callbacks
-#[ext_contract(ext_self)]
-pub trait XCheddar {
-    fn callback_post_unstake(
-        &mut self,
-        sender_id: AccountId,
-        amount: U128,
-        share: U128,
-    );
-}
-
 
